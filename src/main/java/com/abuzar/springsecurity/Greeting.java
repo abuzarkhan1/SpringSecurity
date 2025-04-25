5a14dc3772291c85dc403e388a9a182d876869b9
Abuzar <abuzarkhan1242@gmail.com>
2025-04-25 16:04:07 +0500

    Authorization

--- src/main/java/com/abuzar/springsecurity/Greeting.java
+++ src/main/java/com/abuzar/springsecurity/Greeting.java
@@ -1,5 +1,6 @@
 package com.abuzar.springsecurity;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,17 @@ public class Greeting {
     @GetMapping("/home")
     public String greet() {
         return "Hello World";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String user() {
+        return "Hello user";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String admin() {
+        return "Hello admin";
     }
 }
