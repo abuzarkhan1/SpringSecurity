ba20d86afebceb86d33e3f49d101243c670c1b53
Abuzar <abuzarkhan1242@gmail.com>
2025-04-25 14:19:14 +0500

    in memory authentication

---
+++ src/main/java/com/abuzar/springsecurity/Greeting.java
@@ -0,0 +1,13 @@
+package com.abuzar.springsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Greeting {
+
+    @GetMapping("/home")
+    public String greet() {
+        return "Hello World";
+    }
+}
